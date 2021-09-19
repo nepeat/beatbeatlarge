c244210d325f7a68f5607b651aef90ffb4da1b19
erin <nepeat@gmail.com>
2021-09-19 12:57:54 +0000

    [rust] Add cursed parsing of container metadata.

--- beatbeatlarge_rs/src/file_reader/main.rs
+++ beatbeatlarge_rs/src/file_reader/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
@@ -13,14 +14,42 @@ static GLOBAL: MiMalloc = MiMalloc;
 struct Message {
     timestamp: String,
     hostname: String,
-    message: String
+    message: String,
+    container: Option<BeatContainerMetadata>
+}
+
+struct BeatContainerMetadata {
+    id: String,
+    name: Option<String>,
+    image: Option<String>
+    // labels: HashMap<String, String>
+}
+
+fn json_get_str(data: &simdjson_rust::dom::object::Object, key: &str) -> Option<String> {
+    match data.at_pointer(key) {
+        Ok(value) => Some(value.get_string().unwrap()),
+        Err(e) => None
+    }
 }
 
 fn parse_line(data: simdjson_rust::dom::element::Element) -> Result<Message, Box<dyn std::error::Error>> {
+    let container_meta: Option<BeatContainerMetadata> = match data.at_key("container") {
+        Ok(container_data) => {
+            let container_data = container_data.get_object()?;
+            Some(BeatContainerMetadata {
+                id: container_data.at_key("id")?.get_string()?,
+                name: json_get_str(&container_data, "/name"),
+                image: json_get_str(&container_data, "/image/name")
+            })
+        }
+        Err(_) => None
+    };
+
     let result = Message {
         timestamp: data.at_key("@timestamp")?.get_string()?,
         hostname: data.at_pointer("/host/name")?.get_string()?,
-        message: data.at_key("message")?.get_string()?
+        message: data.at_key("message")?.get_string()?,
+        container: container_meta
     };
 
     Ok(result)
@@ -34,30 +63,50 @@ fn files_read_stream(file_path: &std::path::PathBuf) -> Result<(), Box<dyn std::
     let f = File::open(file_path)?;
     let reader = BufReader::new(f);
 
-    let zstd_stream = zstd::stream::read::Decoder::with_buffer(reader).unwrap();
+    let zstd_stream = zstd::stream::read::Decoder::with_buffer(reader)?;
     let zstd_reader = BufReader::new(zstd_stream);
 
-    println!(
-        "done {} - {} lines, {} secs",
-        file_path.display(),
-        zstd_reader
-        .lines()
-        .map(|line| {
-            let line = line.unwrap_or_default();
-            if !line.is_empty() {
-                let data = parser.parse(&line).unwrap();
-                parse_line(data).unwrap();
+
+    let mut lines = 0;
+    let mut hostmap: HashMap<String, u64> = HashMap::new();
+
+    for line in zstd_reader.lines() {
+        let parsed = match line {
+            Ok(line) => {
+                match parser.parse(&line) {
+                    Ok(data) => {
+                        match parse_line(data) {
+                            Ok(result) => result,
+                            Err(e) => {
+                                eprintln!("Error parsing line. {} {}", line, e);
+                                continue;
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Error parsing JSON in {}: {}", file_path.display(), e);
+                        break;
+                    }
+                }
+            },
+            Err(e) => {
+                eprintln!("Error decompressing in {}: {}", file_path.display(), e);
+                break;
             }
-        })
-        .count(),
+        };
+
+        lines += 1;
+        *hostmap.entry(parsed.hostname).or_insert(0) += 1;
+    }
+
+    println!("done {} - {} lines, {} secs",
+        file_path.display(),
+        lines,
         start_time.elapsed().as_secs_f32()
     );
 
-    // for line in zstd_reader.lines() {
-    //     let line = line.unwrap();
-    //     let v = parser.parse(&line).unwrap();
-    //     let timestamp = v.at_key("@timestamp")?.get_string()?;
-    //     let message = v.at_key("message")?.get_string()?;
+    // for (host, count) in hostmap {
+    //     println!("{}: {}", host, count);
     // }
 
     Ok(())
@@ -75,6 +124,6 @@ fn main() {
 
     filenames.par_iter()
         .map(|x| {
-            files_read_stream(x);
+            files_read_stream(x).unwrap_or_default();
         }).count();
 }
